d0a0b4a7034feb3fc05132851f707427c83113a8
David Moreau-Simard <dms@redhat.com>
2016-05-19 12:54:51 -0400

    Add missing requirements on flask_script and remove migrations
    Migrations will be introduced later, let's keep that out for now

--- ara/manage.py
+++ ara/manage.py
@@ -3,14 +3,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
 
 from ara import app, db
 
-migrate = Migrate(app, db)
-
 manager = Manager(app)
-manager.add_command('db', MigrateCommand)
 
 
 def main():
