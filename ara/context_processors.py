1c99091fa9bacdc90238620bf13626f49f0bfe7c
David Moreau-Simard <dms@redhat.com>
2017-07-20 19:06:09 -0400

    Py3: Change logic to detect whether or not we have records available
    Trying to work with the 'playbooks' variable which ends up being
    different types (object, list, etc.) is troublesome on python 3.
    Change it to detect whether we have something in the database and
    work with that for now.
    
    Change-Id: Ibd9999adda9d2e03f82497ab1ba5cab0b5f1cce9

--- ara/context_processors.py
+++ ara/context_processors.py
@@ -16,6 +16,7 @@ import sys
 
 from ansible import __version__ as ansible_version
 from ara import __release__ as ara_release
+from ara import models
 
 
 def configure_context_processors(app):
@@ -24,10 +25,18 @@ def configure_context_processors(app):
         """
         Returns standard data that will be available in every template view.
         """
+        try:
+            models.Playbook.query.one()
+            empty_database = False
+        except models.MultipleResultsFound:
+            empty_database = False
+        except models.NoResultFound:
+            empty_database = True
 
         # Get python version info
         major, minor, micro, release, serial = sys.version_info
 
         return dict(ara_version=ara_release,
                     ansible_version=ansible_version,
-                    python_version="{0}.{1}".format(major, minor))
+                    python_version="{0}.{1}".format(major, minor),
+                    empty_database=empty_database)
