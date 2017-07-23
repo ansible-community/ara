63e89ee17410b492479aefe52e357427029bc624
David Moreau-Simard <dms@redhat.com>
2017-07-22 22:44:19 -0400

    Bump minimum version of Ansible to 2.2.0.0
    The upstream Ansible community and maintainers support the latest
    two major stable releases. 2.1 is no longer supported, only 2.2
    and 2.3 are.
    
    Change-Id: I164fcc9fefad55c8eaf311f67673711dfa1da549

--- run_tests.sh
+++ run_tests.sh
@@ -123,13 +123,10 @@ export ANSIBLE_LIBRARY="ara/plugins/modules"
 export ARA_DATABASE="${DATABASE}"
 
 # Lint
-# failed.yml does not work with lint due to unicode error
-# https://github.com/willthames/ansible-lint/issues/242
-# include_role is excluded because it is only applied on >2.2 later
-for file in $(find ara/tests/integration ! -path '*failed.yml' ! -path '*include_role.yml' -regex '.*.y[a]?ml'); do
+for file in $(find ara/tests/integration -regex '.*.y[a]?ml'); do
     ansible-lint ${file}
 done
-for file in $(find ara/tests/integration -maxdepth 1 ! -path '*include_role.yml' -regex '.*.y[a]?ml'); do
+for file in $(find ara/tests/integration -maxdepth 1 -regex '.*.y[a]?ml'); do
     ansible-playbook --syntax-check ${file}
 done
 
@@ -144,13 +141,9 @@ ansible-playbook -vv ara/tests/integration/failed.yml || true
 ansible-playbook -vv ara/tests/integration/incomplete.yml &
 sleep 5
 kill $!
-# This playbook leverages include_role that landed in 2.2.0.0
-ansible_version=$(pip freeze |grep ansible== |cut -f3 -d =)
-if [[ $(semver_compare "${ansible_version}" ">=" "2.2.0.0") == "True" ]]; then
-    ansible-playbook --syntax-check ara/tests/integration/include_role.yml
-    ansible-lint ara/tests/integration/include_role.yml
-    ansible-playbook -vv ara/tests/integration/include_role.yml
-fi
+
+# Test include role which is a bit special
+ansible-playbook -vv ara/tests/integration/include_role.yml
 
 # Run test commands
 pbid=$(ara playbook list -c ID -f value |head -n1)
