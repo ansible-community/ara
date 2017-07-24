ecb204230f5466d9a126456e41a95e7a0093d3ac
David Moreau-Simard <dms@redhat.com>
2017-07-24 15:15:27 -0400

    Re-license ARA from ASLv2 to GPLv3
    ARA was originally licensed as Apache 2.0 software to align with
    the license used by the larger community of OpenStack software.
    
    However, ARA is fairly coupled with Ansible, which is GPLv3.
    By maintaining two licenses simultaneously, we may unknowingly infringe
    on the incompatibility of the licenses by importing or linking code
    to and from Ansible software.
    
    In order to avoid the headaches involved in maintaining two licenses
    side by side, remove the ambiguity as to what is the effective license,
    we are relicensing ARA to be fully GPLv3.
    
    Since ARA is not an official OpenStack project, it does not have an
    impact on it's status within the OpenStack community [1] and will
    remain as a hosted project.
    
    We are doing this license change now rather than later as doing this
    change later becomes increasingly complicated as the project gains more
    code contributors.
    
    [1]: http://lists.openstack.org/pipermail/legal-discuss/2017-July/000474.html
    
    Change-Id: I2ec1e0a1d1b38fc74dcb6af2b4f81226d1faa0df

--- ara/plugins/callbacks/log_ara.py
+++ ara/plugins/callbacks/log_ara.py
@@ -1,17 +1,19 @@
-#   Copyright 2017 Red Hat, Inc.
+#  Copyright (c) 2017 Red Hat, Inc.
 #
-#   This module is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
+#  This file is part of ARA: Ansible Run Analysis.
 #
-#   This software is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
+#  ARA is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
 #
-#   You should have received a copy of the GNU General Public License
-#   along with this software.  If not, see <http://www.gnu.org/licenses/>.
+#  ARA is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with ARA.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import (absolute_import, division, print_function)
 
