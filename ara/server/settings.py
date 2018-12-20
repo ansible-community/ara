recordsansible/ara-server

Refactor configuration to use dynaconf
This changes the configuration engine from everett to dynaconf.
dynaconf allows loading configuration from files (json, ini, yaml, toml)
as well as environment variables prefixed by ARA_.

Our usage of dynaconf is similar to the use case from the Pulp [1]
project and they have documented an issue when loading database
parameters [2]. This issue is worked around by importing dynaconf in the
different entry points.

This introduces some other changes as well:
- We're now creating a default configuration and data directory at
  ~/.ara. The location of this directory is controlled with the
  ARA_BASE_DIR environment variable.
- We're now creating a default configuration template in
  ~/.ara/default_config.yaml.
- The default database is now located at ~/.ara/ara.sqlite. The location
  of this database can be customized with the ARA_DATABASE_NAME
  environment variable.
  Note that ARA 0.x used "~/.ara/ansible.sqlite" -- the file name change
  is deliberate in order to avoid user databases clashing between
  versions.

More documentation on this will be available in an upcoming patch.

[1]: https://github.com/pulp/pulp
[2]: https://github.com/rochacbruno/dynaconf/issues/89

Change-Id: I8178b4ca9f2b4d7f4c45c296c08391e84e8b990d

ara/server/settings.py
@@ -1,19 +1,44 @@
 import os
-import sys
+import textwrap
+import yaml
 
-from .utils import EverettEnviron
+# Ensure default base configuration/data directory exists
+BASE_DIR = os.environ.get("ARA_BASE_DIR", os.path.expanduser('~/.ara'))
+if not os.path.isdir(BASE_DIR):
+    os.makedirs(BASE_DIR, mode=0o700)
 
-env = EverettEnviron(DEBUG=(bool, False))
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-SECRET_KEY = env("SECRET_KEY")
-
-DEBUG = env.bool("DEBUG", default=False)
-
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
+# Django built-in server and npm development server
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+CORS_ORIGIN_WHITELIST = ["127.0.0.1:8000", "localhost:3000"]
+CORS_ORIGIN_ALLOW_ALL = True
 
 ADMINS = ()
+
+# Dynaconf Configuration
+SECRET_KEY = True
+GLOBAL_ENV_FOR_DYNACONF = "ARA"
+ENVVAR_FOR_DYNACONF = "ARA_SETTINGS"
+SETTINGS_MODULE_FOR_DYNACONF = "ara.server.settings"
+
+# We're not expecting ARA to use multiple concurrent databases.
+# Make it easier for users to specify the configuration for a single database.
+DATABASE_ENGINE = os.environ.get("ARA_DATABASE_ENGINE", "django.db.backends.sqlite3")
+DATABASE_NAME = os.environ.get("ARA_DATABASE_NAME", os.path.join(BASE_DIR, "ara.sqlite"))
+DATABASE_USER = os.environ.get("ARA_DATABASE_USER", None)
+DATABASE_PASSWORD = os.environ.get("ARA_DATABASE_PASSWORD", None)
+DATABASE_HOST = os.environ.get("ARA_DATABASE_HOST", None)
+DATABASE_PORT = os.environ.get("ARA_DATABASE_PORT", None)
+
+DATABASES = {
+    "default": {
+        "ENGINE": DATABASE_ENGINE,
+        "NAME": DATABASE_NAME,
+        "USER": DATABASE_USER,
+        "PASSWORD": DATABASE_PASSWORD,
+        "HOST": DATABASE_HOST,
+        "PORT": DATABASE_PORT
+    }
+}
 
 INSTALLED_APPS = [
     "django.contrib.auth",
@@ -39,15 +64,6 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-# TODO: Only needed in dev?
-CORS_ORIGIN_ALLOW_ALL = True
-
-# Django built-in server and npm development server
-CORS_ORIGIN_WHITELIST = ("127.0.0.1:8000", "localhost:3000")
-
-ROOT_URLCONF = "ara.server.urls"
-APPEND_SLASH = False
-
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -64,10 +80,6 @@ TEMPLATES = [
     }
 ]
 
-WSGI_APPLICATION = "ara.server.wsgi.application"
-
-DATABASES = {"default": env.db(default="sqlite:///%s" % os.path.join(BASE_DIR, "db.sqlite3"))}
-
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
@@ -75,51 +87,22 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
-LANGUAGE_CODE = "en-us"
-
+USE_TZ = True
 TIME_ZONE = "UTC"
 
 USE_I18N = True
-
 USE_L10N = True
-
-USE_TZ = True
+LANGUAGE_CODE = "en-us"
 
 STATIC_URL = "/static/"
-
 STATIC_ROOT = os.path.join(BASE_DIR, "www", "static")
 
 MEDIA_URL = "/media/"
-
 MEDIA_ROOT = os.path.join(BASE_DIR, "www", "media")
 
-# fmt: off
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {"normal": {"format": "%(asctime)s %(levelname)s %(name)s: %(message)s"}},
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-            "formatter": "normal",
-            "level": env("LOG_LEVEL", default="INFO"),
-            "stream": sys.stdout,
-        }
-    },
-    "loggers": {
-        "ara": {
-            "handlers": ["console"],
-            "level": env("LOG_LEVEL", default="INFO"),
-            "propagate": 0
-        }
-    },
-    "root": {
-        "handlers": ["console"],
-        "level": env("LOG_LEVEL", default="DEBUG")
-    },
-}
-# fmt: on
-
+WSGI_APPLICATION = "ara.server.wsgi.application"
+ROOT_URLCONF = "ara.server.urls"
+APPEND_SLASH = False
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 100,
@@ -135,3 +118,61 @@ REST_FRAMEWORK = {
     ),
     "TEST_REQUEST_DEFAULT_FORMAT": "json",
 }
+
+DEBUG = False
+LOG_LEVEL = "INFO"
+# fmt: off
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {"normal": {"format": "%(asctime)s %(levelname)s %(name)s: %(message)s"}},
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "normal",
+            "level": LOG_LEVEL,
+            "stream": "ext://sys.stdout",
+        }
+    },
+    "loggers": {
+        "ara": {
+            "handlers": ["console"],
+            "level": LOG_LEVEL,
+            "propagate": 0
+        }
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": LOG_LEVEL
+    },
+}
+# fmt: on
+
+DEFAULT_CONFIG = os.path.join(BASE_DIR, "default_config.yaml")
+if not os.path.exists(DEFAULT_CONFIG):
+    CONFIG = dict(
+        BASE_DIR=BASE_DIR,
+        ALLOWED_HOSTS=ALLOWED_HOSTS,
+        CORS_ORIGIN_WHITELIST=CORS_ORIGIN_WHITELIST,
+        CORS_ORIGIN_ALLOW_ALL=CORS_ORIGIN_ALLOW_ALL,
+        SECRET_KEY="please-change-this",
+        DATABASES=DATABASES,
+        STATIC_URL=STATIC_URL,
+        STATIC_ROOT=STATIC_ROOT,
+        MEDIA_URL=MEDIA_URL,
+        MEDIA_ROOT=MEDIA_ROOT,
+        DEBUG=DEBUG,
+        LOG_LEVEL=LOG_LEVEL,
+        LOGGING=LOGGING
+    )
+    with open(DEFAULT_CONFIG, "w+") as config_file:
+        comment = f"""
+        ---
+        # This is a default configuration template generated by ARA.
+        # To use a configuration file such as this one, you need to export the
+        # ARA_SETTINGS configuration variable like so:
+        #   $ export ARA_SETTINGS={DEFAULT_CONFIG}
+
+        """
+        config_file.write(textwrap.dedent(comment))
+        yaml.dump({"default": CONFIG}, config_file, default_flow_style=False)

ansible-community/ara

ara/server/settings.py
@@ -1,19 +1,46 @@
 import os
-import sys
+import textwrap
 
-from .utils import EverettEnviron
+import yaml
 
-env = EverettEnviron(DEBUG=(bool, False))
+# Ensure default base configuration/data directory exists
+BASE_DIR = os.environ.get("ARA_BASE_DIR", os.path.expanduser("~/.ara"))
+SERVER_DIR = os.path.join(BASE_DIR, "server")
+if not os.path.isdir(SERVER_DIR):
+    os.makedirs(SERVER_DIR, mode=0o700)
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-SECRET_KEY = env("SECRET_KEY")
-
-DEBUG = env.bool("DEBUG", default=False)
-
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
+# Django built-in server and npm development server
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+CORS_ORIGIN_WHITELIST = ["127.0.0.1:8000", "localhost:3000"]
+CORS_ORIGIN_ALLOW_ALL = True
 
 ADMINS = ()
+
+# Dynaconf Configuration
+SECRET_KEY = True
+GLOBAL_ENV_FOR_DYNACONF = "ARA"
+ENVVAR_FOR_DYNACONF = "ARA_SETTINGS"
+SETTINGS_MODULE_FOR_DYNACONF = "ara.server.settings"
+
+# We're not expecting ARA to use multiple concurrent databases.
+# Make it easier for users to specify the configuration for a single database.
+DATABASE_ENGINE = os.environ.get("ARA_DATABASE_ENGINE", "django.db.backends.sqlite3")
+DATABASE_NAME = os.environ.get("ARA_DATABASE_NAME", os.path.join(SERVER_DIR, "ansible.sqlite"))
+DATABASE_USER = os.environ.get("ARA_DATABASE_USER", None)
+DATABASE_PASSWORD = os.environ.get("ARA_DATABASE_PASSWORD", None)
+DATABASE_HOST = os.environ.get("ARA_DATABASE_HOST", None)
+DATABASE_PORT = os.environ.get("ARA_DATABASE_PORT", None)
+
+DATABASES = {
+    "default": {
+        "ENGINE": DATABASE_ENGINE,
+        "NAME": DATABASE_NAME,
+        "USER": DATABASE_USER,
+        "PASSWORD": DATABASE_PASSWORD,
+        "HOST": DATABASE_HOST,
+        "PORT": DATABASE_PORT,
+    }
+}
 
 INSTALLED_APPS = [
     "django.contrib.auth",
@@ -39,15 +66,6 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-# TODO: Only needed in dev?
-CORS_ORIGIN_ALLOW_ALL = True
-
-# Django built-in server and npm development server
-CORS_ORIGIN_WHITELIST = ("127.0.0.1:8000", "localhost:3000")
-
-ROOT_URLCONF = "ara.server.urls"
-APPEND_SLASH = False
-
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -64,10 +82,6 @@ TEMPLATES = [
     }
 ]
 
-WSGI_APPLICATION = "ara.server.wsgi.application"
-
-DATABASES = {"default": env.db(default="sqlite:///%s" % os.path.join(BASE_DIR, "db.sqlite3"))}
-
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
@@ -75,51 +89,22 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
-LANGUAGE_CODE = "en-us"
-
+USE_TZ = True
 TIME_ZONE = "UTC"
 
 USE_I18N = True
-
 USE_L10N = True
-
-USE_TZ = True
+LANGUAGE_CODE = "en-us"
 
 STATIC_URL = "/static/"
-
-STATIC_ROOT = os.path.join(BASE_DIR, "www", "static")
+STATIC_ROOT = os.path.join(SERVER_DIR, "www", "static")
 
 MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(SERVER_DIR, "www", "media")
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "www", "media")
-
-# fmt: off
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {"normal": {"format": "%(asctime)s %(levelname)s %(name)s: %(message)s"}},
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-            "formatter": "normal",
-            "level": env("LOG_LEVEL", default="INFO"),
-            "stream": sys.stdout,
-        }
-    },
-    "loggers": {
-        "ara": {
-            "handlers": ["console"],
-            "level": env("LOG_LEVEL", default="INFO"),
-            "propagate": 0
-        }
-    },
-    "root": {
-        "handlers": ["console"],
-        "level": env("LOG_LEVEL", default="DEBUG")
-    },
-}
-# fmt: on
-
+WSGI_APPLICATION = "ara.server.wsgi.application"
+ROOT_URLCONF = "ara.server.urls"
+APPEND_SLASH = False
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 100,
@@ -135,3 +120,62 @@ REST_FRAMEWORK = {
     ),
     "TEST_REQUEST_DEFAULT_FORMAT": "json",
 }
+
+DEBUG = False
+LOG_LEVEL = "INFO"
+# fmt: off
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {"normal": {"format": "%(asctime)s %(levelname)s %(name)s: %(message)s"}},
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "normal",
+            "level": LOG_LEVEL,
+            "stream": "ext://sys.stdout",
+        }
+    },
+    "loggers": {
+        "ara": {
+            "handlers": ["console"],
+            "level": LOG_LEVEL,
+            "propagate": 0
+        }
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": LOG_LEVEL
+    },
+}
+# fmt: on
+
+# TODO: Split this out to a CLI command (django-admin command ?)
+DEFAULT_CONFIG = os.path.join(SERVER_DIR, "default_config.yaml")
+if not os.path.exists(DEFAULT_CONFIG):
+    CONFIG = dict(
+        BASE_DIR=BASE_DIR,
+        ALLOWED_HOSTS=ALLOWED_HOSTS,
+        CORS_ORIGIN_WHITELIST=CORS_ORIGIN_WHITELIST,
+        CORS_ORIGIN_ALLOW_ALL=CORS_ORIGIN_ALLOW_ALL,
+        SECRET_KEY="please-change-this",
+        DATABASES=DATABASES,
+        STATIC_URL=STATIC_URL,
+        STATIC_ROOT=STATIC_ROOT,
+        MEDIA_URL=MEDIA_URL,
+        MEDIA_ROOT=MEDIA_ROOT,
+        DEBUG=DEBUG,
+        LOG_LEVEL=LOG_LEVEL,
+        LOGGING=LOGGING,
+    )
+    with open(DEFAULT_CONFIG, "w+") as config_file:
+        comment = f"""
+        ---
+        # This is a default configuration template generated by ARA.
+        # To use a configuration file such as this one, you need to export the
+        # ARA_SETTINGS configuration variable like so:
+        #   $ export ARA_SETTINGS={DEFAULT_CONFIG}
+
+        """
+        config_file.write(textwrap.dedent(comment))
+        yaml.dump({"default": CONFIG}, config_file, default_flow_style=False)