62c2da3845c07c7a04ae934df257822163b9062f
David Moreau Simard <dmsimard@redhat.com>
2020-01-30 15:59:11 -0500

    ui: Add a default robots.txt to prevent bot crawling
    Search engines shouldn't be indexing the contents of ARA.
    
    Change-Id: I110056b91a969cd6f714917ae7bda078f1a99be6

--- ara/ui/urls.py
+++ ara/ui/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from ara.ui import views
 
 app_name = "ui"
 urlpatterns = [
     path("", views.Index.as_view(), name="index"),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
     path("playbook/<int:pk>.html", views.Playbook.as_view(), name="playbook"),
     path("result/<int:pk>.html", views.Result.as_view(), name="result"),
     path("file/<int:pk>.html", views.File.as_view(), name="file"),
