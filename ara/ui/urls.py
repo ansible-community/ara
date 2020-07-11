d247f6c0f8eb5f4b96f949c6f41df2d4d2f25a91
David Moreau Simard <moi@dmsimard.com>
2020-07-11 17:26:51 -0400

    UI: Pluralize URLs for resources to match API
    When querying the API for playbooks, the url would be:
        /api/v1/playbooks
    And when querying the details of a playbook, the url would be:
        /api/v1/playbooks/<id>
    
    With that in mind, it makes sense to align the UI so that when viewing
    the details of a playbook, the url would be:
        /playbooks/1
    instead of:
        /playbook/1
    
    Change-Id: I3e7ee6a25e5f15518208b0b0a5f46c61e5ca89cb

--- ara/ui/urls.py
+++ ara/ui/urls.py
@@ -7,9 +7,9 @@ app_name = "ui"
 urlpatterns = [
     path("", views.Index.as_view(), name="index"),
     path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
-    path("playbook/<int:pk>.html", views.Playbook.as_view(), name="playbook"),
-    path("result/<int:pk>.html", views.Result.as_view(), name="result"),
-    path("file/<int:pk>.html", views.File.as_view(), name="file"),
-    path("host/<int:pk>.html", views.Host.as_view(), name="host"),
-    path("record/<int:pk>.html", views.Record.as_view(), name="record"),
+    path("playbooks/<int:pk>.html", views.Playbook.as_view(), name="playbook"),
+    path("results/<int:pk>.html", views.Result.as_view(), name="result"),
+    path("files/<int:pk>.html", views.File.as_view(), name="file"),
+    path("hosts/<int:pk>.html", views.Host.as_view(), name="host"),
+    path("records/<int:pk>.html", views.Record.as_view(), name="record"),
 ]
