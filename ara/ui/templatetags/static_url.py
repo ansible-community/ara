ae525958a771828699d1a25b4ef80334b3ca9f64
David Moreau Simard <moi@dmsimard.com>
2021-03-17 14:50:19 -0400

    Explain what the static_url filter is meant for

--- ara/ui/templatetags/static_url.py
+++ ara/ui/templatetags/static_url.py
@@ -8,6 +8,12 @@ register = template.Library()
 
 @register.simple_tag(name="static_url", takes_context=True)
 def static_url(context, url):
+    """
+    The statically generated version of the website (via `ara-manage generate`) needs to have relative links instead of
+    absolute links in order to let it be hosted under any directory, for example http://logserver/some/path/ara-report.
+    This filter adapts links from the django URL tag so they are more like "playbooks/<id>.html" and less like
+    "/playbooks/<id>.html".
+    """
     if not context["static_generation"]:
         return url
 
