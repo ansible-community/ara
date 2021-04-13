0db7c3d344708defb60441e110d01691ce4c014e
Guillaume Vincent <gvincent@redhat.com>
2021-04-13 09:44:59 +0200

    Add about modal
    Add a About link button in the header to display a modal with version
    number
    
    https://github.com/ansible-community/ara/issues/248

---
+++ ara/server/context_processors.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2021 The ARA Records Ansible authors
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+import pbr.version
+
+from django.conf import settings
+
+
+def about(request):
+    return {"ARA_VERSION": pbr.version.VersionInfo("ara").release_string()}
