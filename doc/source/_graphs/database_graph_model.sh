58a5ee300bf3272d87e466b24cbdcc3d5361d9e1
David Moreau Simard <moi@dmsimard.com>
2021-07-16 13:48:24 -0400

    docs: Add a database model graph
    This is an addition to help flesh out the docs about relationships
    between objects and a quick win since it works out of the box with
    django_extensions' graph_models.
    
    Since the database model doesn't move very much, it's rendered a single
    time manually (instead of, say, automatically after every commit) and
    there are instructions included to generate it again, if need be, in order
    to avoid adding django_extensions as a dependency.

---
+++ doc/source/_graphs/database_graph_model.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Copyright (c) 2021 The ARA Records Ansible authors
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+# To generate a database graph:
+# - Install django_extensions (i.e, pip install django_extensions)
+# - Uncomment `django_extensions` from INSTALLED_APPS in ara/server/settings.py (not settings.yaml!)
+# - Install pygraphviz (on Fedora, dnf install graphviz-devel && pip install pygraphviz)
+
+# More documentation about django_extensions and graph_models:
+# - https://django-extensions.readthedocs.io/en/latest/installation_instructions.html
+# - https://django-extensions.readthedocs.io/en/latest/graph_models.html
+
+ara-manage graph_models api --pygraphviz \
+    --group-models \
+    --theme django2018 \
+    --arrow-shape normal \
+    --layout dot \
+    --verbose-names \
+    -o database-model.png
