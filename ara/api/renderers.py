064bdaf2be8c52ef3c642e79b62313cde3083f50
Christoph Hille <hille721@gmail.com>
2021-08-07 11:21:23 +0200

    added docstring with explanation and usage example

--- ara/api/renderers.py
+++ ara/api/renderers.py
@@ -2,7 +2,30 @@ from rest_framework.renderers import BrowsableAPIRenderer
 
 
 class BrowsableAPIRendererWithoutForms(BrowsableAPIRenderer):
-    """Renders the browsable api, but excludes the forms."""
+    """
+    Renders the browsable api, but excludes the forms.
+
+    If you are running ara in production you will collect a lot of data over time.
+    This will slow down the browsable API
+    because of the population of related keys into the dropdowns of the html POST/PUT form.
+    This is noticed most strongly in the results API view (/api/v1/results or /api/v1/results/X)
+    because there we have a lot of related fields (delegated_to, host, task, play, playbook).
+
+    Using this renderer fixes issue #323.
+
+    To enable this renderer it has to be set in `server/settings.py`:
+
+        REST_FRAMEWORK = {
+            ...
+            "DEFAULT_RENDERER_CLASSES": (
+                ...
+                "ara.api.renderers.BrowsableAPIRendererWithoutForms",
+                ...
+            ),
+            ...
+        }
+
+    """
 
     def get_rendered_html_form(self, data, view, method, request):
         return None
