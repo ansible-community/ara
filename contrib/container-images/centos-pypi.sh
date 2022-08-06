a65dbd65fe4a30478ac1ef4a078534852b755fbe
David Moreau Simard <moi@dmsimard.com>
2022-08-06 00:19:46 -0400

    contrib: overhaul container image scripts
    - Add a centos-source.sh script so we can test from source in addition
      to PyPI
    - Install everything from PyPI (except ara when from source) in order
      to avoid mixing distribution packages with PyPI packages
    - Some of the dependencies we install from PyPI have development
      libraries that must now be installed, do it but undo it to clean up
    - Only go over bash when necessary, otherwise it can lead to weird
      parsing of commands and arguments passed via podman
    - Update CI and docs accordingly
    - Actually keep centos stream 8 for the pypi image due a tzlocal issue
      with stream9

--- contrib/container-images/centos-pypi.sh
+++ contrib/container-images/centos-pypi.sh
@@ -2,23 +2,30 @@
 # Copyright (c) 2022 The ARA Records Ansible authors
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-# Builds an ARA API server container image using the latest PyPi packages on CentOS Stream 9.
-build=$(buildah from quay.io/centos/centos:stream9)
+# Builds an ARA API server container image using the latest PyPi packages on CentOS Stream 8.
+# TODO: update to stream9 once 1.6.0 is released https://github.com/ansible-community/ara/issues/401
+build=$(buildah from quay.io/centos/centos:stream8)
 
-# Get all updates, install pip, database backends and gunicorn application server
-# This lets users swap easily from the sqlite default to mysql or postgresql just by tweaking settings.yaml.
-# Note: We use the packaged versions of psycopg2 and mysql python libraries so
-#       we don't need to install development libraries before installing them from PyPi.
-buildah run "${build}" -- /bin/bash -c "dnf update -y && dnf install -y epel-release && dnf install -y which python3-pip python3-gunicorn python3-psycopg2 python3-mysql && dnf clean all"
+# Ensure everything is up to date and install requirements
+buildah run "${build}" -- /bin/bash -c "dnf update -y && dnf install -y which python3-pip python3-pip-wheel"
 
-# Install ara from source with API server extras for dependencies (django & django-rest-framework)
-buildah run "${build}" -- /bin/bash -c "pip3 install ara[server]"
+# Install development dependencies in a single standalone transaction so we can fully undo it later
+# without leaving dangling uninstalled dependencies
+buildah run "${build}" -- dnf install -y python3-devel gcc postgresql postgresql-devel mariadb-connector-c-devel
+
+# Install ara from PyPI with API server extras for dependencies (django & django-rest-framework)
+# including database backend libraries and gunicorn
+buildah run "${build}" -- python3 -m pip install "ara[server]" "psycopg2-binary<2.9" mysqlclient gunicorn
+
+# Remove development dependencies and clean up
+transaction=$(buildah run "${build}" -- /bin/bash -c "dnf history | grep python3-devel | awk '{print \$1}'")
+buildah run "${build}" -- /bin/bash -c "dnf history undo -y ${transaction} && dnf clean all"
 
 # Set up the container to execute SQL migrations and run the API server with gunicorn
 # Temporarily set $TZ env variable pending release of 1.6.0 with timezone fixes
 buildah config --env TZ=UTC
 buildah config --env ARA_BASE_DIR=/opt/ara "${build}"
-buildah config --cmd "bash -c '/usr/local/bin/ara-manage migrate && /usr/bin/gunicorn-3 --workers=4 --access-logfile - --bind 0.0.0.0:8000 ara.server.wsgi'" "${build}"
+buildah config --cmd "bash -c '/usr/local/bin/ara-manage migrate && /usr/local/bin/gunicorn --workers=4 --access-logfile - --bind 0.0.0.0:8000 ara.server.wsgi'" "${build}"
 buildah config --port 8000 "${build}"
 
 # Commit this container to an image name
