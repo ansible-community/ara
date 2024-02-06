19a38c7219a7e6b77b0f904e8366c01502b3e44e
David Moreau Simard <moi@dmsimard.com>
2024-02-06 05:01:02 -0500

    tests: don't use pkg_resources to determine version of ara
    We're already using pbr to get the version of ara so use that instead
    since some distributions no longer install setuptools by default.

--- ara/api/tests/tests_utils.py
+++ ara/api/tests/tests_utils.py
@@ -1,8 +1,9 @@
 # Copyright (c) 2022 The ARA Records Ansible authors
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-import pkg_resources
 from rest_framework.test import APITestCase
+
+from ara.setup import ara_version as ARA_VERSION
 
 
 class RootTestCase(APITestCase):
@@ -10,6 +11,6 @@ class RootTestCase(APITestCase):
         result = self.client.get("/api/")
         self.assertEqual(set(result.data.keys()), {"kind", "version", "api"})
         self.assertEqual(result.data["kind"], "ara")
-        self.assertEqual(result.data["version"], pkg_resources.get_distribution("ara").version)
+        self.assertEqual(result.data["version"], ARA_VERSION)
         self.assertEqual(len(result.data["api"]), 1)
         self.assertTrue(result.data["api"][0].endswith("/api/v1/"))
