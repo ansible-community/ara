63199341aaea5b8002685e6f158cd8d1d2f52b75
David Moreau Simard <moi@dmsimard.com>
2025-07-20 20:07:18 -0400

    CI: Migrate jobs from GitHub/Zuul to Forgejo actions
    This removes the implementation of CI jobs with Zuul and includes
    all the plumbing to make our existing jobs run on Codeberg/forgejo:
    1) docs, linters, unit and integration tests
    2) container image build & tests
    
    The first run inside a Fedora 42 container image.
    This image is built with buildah and published to the repository.
    
    The second creates an OpenStack VM (Fedora 42 or Debian 12) and then
    runs the builds and tests there.
    
    In addition, where relevant, we are now sending ansible playbook results
    to demo.recordsansible.org for reporting.

---
+++ contrib/container-images/fedora-ci-image.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+# Copyright (c) 2025 The ARA Records Ansible authors
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+# This image is meant to be used for exercising CI tests on Codeberg/Forgejo
+build=$(buildah from quay.io/fedora/fedora:42)
+
+# Ensure everything is up to date and install requirements
+buildah run "${build}" -- /bin/bash -c "dnf update -y && dnf install -y git awk hostname nodejs nodejs20 tox which python3 python3-pip"
+
+# Commit this container to an image name
+buildah commit "${build}" "${1:-$USER/fedora-ci-image}"
